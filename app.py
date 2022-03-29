c2d0f074e5872fef00cf08375cd54115e1f24a58
JackLee9437 <iacog@kakao.com>
2022-03-30 01:57:27 +0900

    키워드 - 회원기능 개선 ver 1.1
    변경사항 :
    
    # 키워드: 생성, 수정, 추가,고치기,문서화,스타일, 테스트
    # "왜", "무엇"을 포함하기
    # 제목은 80자 이내로, 긴 내용은 줄바꿈하고 본문에서

--- app.py
+++ app.py
@@ -5,16 +5,22 @@ from wsgiref.util import request_uri
 from pymongo import MongoClient
 from flask import Flask, render_template, jsonify, request
 from flask_jwt_extended import *
-import bcrypt, datetime
+import bcrypt, datetime, time
+from flask_jwt_extended.config import config
 
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.dbjungle
 
+times = ["06:00","07:00","08:00"]
+types = ["헬스", "러닝", "산책"]
+user = ""
+
 app.config.update(
     DEBUG=True,
-    JWT_SECRET_KEY="JUNGLERSSPORTS"
+    JWT_SECRET_KEY="JUNGLERSSPORTS",
+    JWT_TOKEN_LOCATION=["headers", "cookies"]
 )
 
 jwt = JWTManager(app)
@@ -28,11 +34,48 @@ def check_if_token_is_revoked(jwt_header, jwt_payload) :
 # HTML 화면 보여주기 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    homeDict = dict()
+    for time in times:
+        homeDict[time] = list()
+        for type in types:
+            players = []
+            for player in list(db.user.find({"time":time,"type":type})):
+                players.append(player['username'])
+            homeDict[time].append({"type":type, "players":players})
+
+    jwtToken = request.cookies.get('jwt-token')
+    if jwtToken is None : return render_template('index.html', loginChecked = "true", homeDict = homeDict)
+
+    jti = decode_token(jwtToken)['jti']
+    user = decode_token(jwtToken).get(config.identity_claim_key, None)
+    loginChecked = jti in jwt_blocklist
+
+    return render_template('index.html', homeDict = homeDict, loginChecked = loginChecked, username = user)
+
+    
+
 
 @app.route('/mypage')
-def show_result():
-    return render_template('mypage.html')
+def show_mypage():
+    jwtToken = request.cookies.get('jwt-token')
+    if jwtToken is None : return render_template('mypage.html', loginChecked = True)
+
+    jti = decode_token(jwtToken)['jti']
+    user = decode_token(jwtToken).get(config.identity_claim_key, None)
+    loginChecked = jti in jwt_blocklist
+    
+    loginUser = db.user.find_one({"userid": user})
+    registeredType = loginUser['type']
+    registeredTime = loginUser['time']
+    
+    players = []
+    for player in list(db.user.find({"time":registeredTime,"type":registeredType})):
+        players.append(player['username'])
+    result = {"type" : registeredType, "time" : registeredTime, "players" : players}
+    print(result)
+    
+    return render_template('mypage.html', loginChecked = loginChecked, username = user, result = result)
+
 
 @app.route('/login')
 def show_login():
@@ -44,40 +87,10 @@ def show_signup():
 
 # API 역할을 하는 부분
 
-@app.route('/api/list', methods=['GET'])
-def show_member_counts():
-    time_receive = request.form['time_give'] # 1. 클라이언트가 전달한 time_give 변수를 time_receive 변수에 넣음
-    type_receive = request.form['type_give'] # 2. 클라이언트가 전달한 type_give 변수를 type_receive 변수에 넣음
-
-    # 1. db에서 위의 조건을 바탕으로 DB 목록 전체를 검색해서, time과 type가 일치하는 인원 수를 가져옵니다.
-    counts = len(list(db.service.find_one({"time":time_receive, "type":type_receive}, {"_id":False})))
-    # 2. 성공하면 success 메시지와 함께 counts 라는 운동 인원 수를 클라이언트에 전달합니다.
-    return jsonify({'result': 'success', 'counts': counts, 'msg':'운동인원 불러오기 완료!'})
-
 # 운동하기 클릭했을 경우 DB 업데이트
-@app.route('/api/add', methods=['POST'])
-def add():
-    name_receive = request.form['name_give']# 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-    new_time_receive = request.form['new_time_give']
-    new_type_receive = request.form['new_type_give']
-    target_user = db.service.find_one({"name":name_receive})# 2. service 목록에서 find_one으로 name이 name_receive와 일치하는 user를 찾습니다.
-    db.service.update_one({"name":name_receive}, {'$set':{"type":new_type_receive}})
-    db.service.update_one({"name":name_receive}, {'$set':{"time":new_time_receive}})
-
-    return jsonify({'result': 'success', 'msg':'운동하기 신청되었습니다!'})
-
-
-@app.route('/api/cancel', methods=['POST'])
-def delete_count():
-    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
-    name_receive = request.form['name_give']
-    # 2. mystar 목록에서 delete_one으로 name이 name_receive와 일치하는 star를 제거합니다.
-    db.mystar.delete_one({"name":name_receive})
-    # 3. 성공하면 success 메시지를 반환합니다.
-    return jsonify({'result': 'success', 'msg': '삭제 완료! 안녕!'})
     
 #회원가입기능
-@app.route('/signup', methods=("POST"))
+@app.route('/signup', methods=["POST"])
 def signup():
     inputData = request.form
     
@@ -138,12 +151,17 @@ def user_logout() :
 @jwt_required()
 def register() :
     user = get_jwt_identity()
+    
     if user is None :
         return jsonify({"result": "forbidden", "msg": "로그인 해주세요!"})
     else :
-        return user
-
-
+        time_receive = request.form['time_give'] # 1. 클라이언트가 전달한 time_give 변수를 time_receive 변수에 넣음
+        type_receive = request.form['type_give'] # 2. 클라이언트가 전달한 type_give 변수를 type_receive 변수에 넣음
+        print("case1")
+        db.user.update_one({'userid':user},{'$set':{"time":time_receive,"type":type_receive}})
+        print("case2")
+        # 2. 성공하면 success 메시지와 함께 counts 라는 운동 인원 수를 클라이언트에 전달합니다.
+        return jsonify({'result': 'success', 'msg':'참가 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
