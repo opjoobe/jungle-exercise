2e1385d3a2e9bf4de7165a790c93bf78139ea6d7
JackLee9437 <iacog@kakao.com>
2022-03-31 12:33:35 +0900

    scheduler에 comment reset 추가

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -56,7 +56,7 @@ def show_reset():
                 print("player {} 총 운동 횟수: {}회 - 운동한 날: {}".format(user_name, len(player['log']), list(player['log'].keys())))
                 '''
             # 이렇게 for문을 다 돌려서 log를 업데이트 해줬으면, time과 type 필드를 제거해줌. 초기화 완료!
-            db.user.update_many({},{'$unset':{'time':True, 'type':True}})
+            db.user.update_many({},{'$unset':{'time':True, 'type':True, 'comment':True}})
             return jsonify({"result":"초기화 완료"})
         else:
             return jsonify({"result":"등록한 사람이 없습니다."})
